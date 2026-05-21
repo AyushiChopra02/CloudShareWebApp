6cebbad95a6cfd7bbdaab5f53139979eb881f7ff
AyushiChopra02 <ayushichopra.02@gmail.com>
2026-05-21 16:34:08 +0530

    fix frontend production api

--- cloudShareBackend/wait-for-mysql.sh
+++ cloudShareBackend/wait-for-mysql.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-echo "Waiting for MySQL at mysql-container:3306..."
-until nc -z mysql-container 3306; do
-  sleep 2
-done
-echo "MySQL is up! Starting backend..."
+
+echo "Starting backend..."
 exec java -jar /app/app.jar
