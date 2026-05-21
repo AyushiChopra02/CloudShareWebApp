5c4dff12bdd4c03f5b3d44f18383ed8d6b87a95c
AyushiChopra02 <ayushichopra.02@gmail.com>
2026-05-21 16:52:12 +0530

    add spring security cors config

--- cloudShareBackend/src/main/java/com/cloudshare/config/SecurityConfig.java
+++ cloudShareBackend/src/main/java/com/cloudshare/config/SecurityConfig.java
@@ -2,24 +2,23 @@ package com.cloudshare.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    http
-        .cors(cors -> {
-        })
-        .csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-            .anyRequest().permitAll());
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
 
-    return http.build();
-  }
+        return http.build();
+    }
 }
